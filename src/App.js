395bafd54971d39f6837aadd5773f582a3b40d2c
Gustavo Francisco <ciscosweater@gmail.com>
2023-03-14 12:08:39 -0300

    gh-pages fix

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Home from "./pages/home";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/cart' element={<CartPage />} />
