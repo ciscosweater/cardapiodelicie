4f861630112564bedb666846d2ce6201c21cc805
Gustavo Francisco <ciscosweater@gmail.com>
2023-03-23 16:37:41 -0300

    scroll to top added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { useEffect } from "react";
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
 import CartPage from "./pages/cart";
 import Home from "./pages/home";
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
+
 
 function App() {
   return (
     <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <ScrollToTop />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/cart' element={<CartPage />} />
